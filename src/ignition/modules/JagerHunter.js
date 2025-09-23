9d042fbe092e57e0ec74bfa29699eca2a6338664
Coderdivine <chimdi4332@gmail.com>
2025-09-23 17:12:52 +0100

    Little Pengu contracts: mainnet

--- src/ignition/modules/JagerHunter.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("JagerHunterModule", (m) => {
-  const jagerHunter = m.contract("JagerHunter");
-
-  return { jagerHunter };
-});
